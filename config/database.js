3e02f247cac08bd0184d27483317aa2c063e3f83
aymanzriek <124799964+aymanzriek@users.noreply.github.com>
2023-06-13 09:13:27 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize(
+const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS, {
+  process.env.DB_PASS,
+  {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Add this line to disable SSL verification
+      },
+    },
   }
 );
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err));
+
+module.exports = sequelize;
